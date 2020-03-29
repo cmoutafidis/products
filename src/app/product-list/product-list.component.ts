8c30195796e44267134952a17cf90f23db5616f5
Charis <programatisths@gmail.com>
2020-03-29 16:44:04 +0300

    Added product component and Input functionality on product component

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,18 +1,18 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
-import { products } from '../products';
+import {Product, products} from '../products';
 
 @Component({
-  selector: 'app-product-list',
-  templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+    selector: 'app-product-list',
+    templateUrl: './product-list.component.html',
+    styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent {
-  products = products;
+    products: Product[] = products;
 
-  share() {
-    window.alert('The product has been shared!');
-  }
+    share() {
+        window.alert('The product has been shared!');
+    }
 }
 
 
